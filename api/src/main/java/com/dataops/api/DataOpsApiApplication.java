1eb5eef88a702f12eb518e71db038f84afa88897
nickunger99 <nicolas.unger@yahoo.com.br>
2024-09-15 16:46:12 -0300

    feat: spring initializr project with some dependencies

---
+++ api/src/main/java/com/dataops/api/DataOpsApiApplication.java
@@ -0,0 +1,13 @@
+package com.dataops.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataOpsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataOpsApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/dataops/api/DataOpsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataops.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataOpsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
